9b8f7a58309a143e5bd709c3169ea6285a42b89b
fredrawi <fredrikandreas03@gmail.com>
2026-09-23 11:19:26 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  return (
+    <div>
+      Hello {value}
+      <h2>New header</h2>
+    </div>
+  );
 }
 
 export default App;
